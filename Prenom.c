b34dc64af29d6da02c5553183145ea626ce0d2c0
Horsell <ihorsell@gmail.com>
2015-05-31 18:59:20 +0200

    Ajout de la fonction 'hl_prenom' dans le programme Prenom.c

--- Prenom.c
+++ Prenom.c
@@ -9,3 +9,14 @@ int		main(int argc, char *argv[])
 	return 0;
 }
 
+int		hl_prenom()
+{
+	char	prenom[100];
+
+	printf("\n");
+	printf("Bonjour... Quelle est votre prenom ?\n\n");
+	scanf("%s", prenom);
+	printf("Bonjour %s,\nBienvenue dans ce mini programme.\n\n", prenom);
+
+	return 0;
+}
