db72b0a209a124c10d65d2417ea555b7eb5dbfd6
Horsell <ihorsell@gmail.com>
2015-05-31 18:55:36 +0200

    Ajout du prohramme Helloworld.c

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int		hl_hello();
+
+int		main(char argc, char *argv[])
+{
+	hl_hello();
+
+	return 0;
+}
+
+int 	hl_hello()
+{
+	printf("Hello World !\n");
+
+	return 0;
+}
